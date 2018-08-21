effa1cf0088ccab488c003ad8a40bfebb359ddc3
droid <raghav.sharma848@gmail.com>
2018-08-21 15:27:49 +0530

    cleaned up the code

--- app.js
+++ app.js
@@ -6,30 +6,6 @@ const promises = require('./routes/routes.promises');
 
 app.use('/callback',callbacks);
 app.use('/promise',promises);
-// let url = 'https://api.github.com/users/narenaryan';
-
-// function getData(url, cb) {
-//     // Setting URL and headers for request
-//     var options = {
-//         url: url,
-//         headers: {
-//             'User-Agent': 'request'
-//         }
-//     };
-//     request.get(options, function(err, resp, body){
-//         console.log(body);
-//         cb(err, body)
-//     })
-
-
-// }
-
-// app.get('/',function(req,res){
-//     getData(url, function(err, body){
-//         res.send(body)
-//     })
-//     console.log(res);
-// })
 
 app.listen(8080,function(){
     console.log('server started !');
